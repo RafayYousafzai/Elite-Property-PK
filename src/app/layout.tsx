9553818a7af93a7e3108b663b91e81352f8610ae
Rafay <rafaykhan.mesh@gmail.com>
2025-10-11 20:17:53 +0500

    feat: add Google Tag Manager integration for enhanced tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-TR6MVDDH');
+          `}
+        </Script>
+        {/* End Google Tag Manager */}
+
         {/* Meta Pixel Code */}
         <Script id="facebook-pixel" strategy="afterInteractive">
           {`
@@ -51,6 +63,17 @@ export default function RootLayout({
         </noscript>
       </head>
       <body className={`${font.className} bg-white dark:bg-black antialiased`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TR6MVDDH"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         <NextTopLoader color="#d8b648" showSpinner={false} />
         <SessionProviderComp session={session}>
           <ThemeProvider
