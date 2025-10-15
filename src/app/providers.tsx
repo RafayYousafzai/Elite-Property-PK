9b3b4741bfd93856be14f700d38d630f862cccfa
Rafay <rafaykhan.mesh@gmail.com>
2025-10-15 11:47:08 +0500

    Refactor code structure for improved readability and maintainability

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,19 +1,22 @@
 "use client";
 
 // app/providers.tsx
-
 import { HeroUIProvider } from "@heroui/react";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 import SmoothScrollProvider from "@/components/scrolling/SmoothScrollProvider";
 import { CustomChatWidget } from "@/components/ChatWidget";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <HeroUIProvider>
-      <SmoothScrollProvider>
-        <CustomChatWidget />
-        {children}
-      </SmoothScrollProvider>
-    </HeroUIProvider>
+    <>
+      <Analytics />
+      <HeroUIProvider>
+        <SmoothScrollProvider>
+          <CustomChatWidget />
+          {children}
+        </SmoothScrollProvider>
+      </HeroUIProvider>
+    </>
   );
 }
