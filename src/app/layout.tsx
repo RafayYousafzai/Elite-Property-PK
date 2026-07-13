ef78e4ce47c9dea0c819265c2b0045d2c4f0ba81
Rafay Khan <rafaykhan.mesh@gmail.com>
2026-07-13 20:48:50 +0500

    fix: add test event code to Meta Pixel tracking configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,6 +38,7 @@ export default function RootLayout({
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
             fbq('init', '1763559634157665');
+            fbq('set', 'test_event_code', 'TEST6577');
             fbq('track', 'PageView');
           `}
         </Script>
